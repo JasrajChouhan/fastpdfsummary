e8224f575ceb9d47fb1567d75e3b68d96b6e463c
Jasraj Chouhan <jasrajc945@gmail.com>
2025-04-03 19:50:42 +0530

    feat(ui): Add demo-section on home.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import BGGradient from '@/components/commons/BGGradient';
+// import BGGradient from '@/components/commons/BGGradient';
+import { DemoSection } from '@/components/home/demo-section';
 
 import { HeroSection } from '@/components/home/hero-section';
 
 export default function Home() {
   return (
     <main className="w-full relative">
-      <HeroSection />
-      <BGGradient />
+      {/* <BGGradient /> */}
+      <div className="flex flex-col">
+        <HeroSection />
+        <DemoSection />
+      </div>
     </main>
   );
 }
