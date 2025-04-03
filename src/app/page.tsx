ba4d140dd193883dd4c842c65992228e753760ec
Jasraj Chouhan <jasrajc945@gmail.com>
2025-04-03 22:09:53 +0530

    feat(ui): add how-it-works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 // import BGGradient from '@/components/commons/BGGradient';
+import { HowItWorks } from '@/components/commons/how-it-works';
 import { DemoSection } from '@/components/home/demo-section';
 
 import { HeroSection } from '@/components/home/hero-section';
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="flex flex-col">
         <HeroSection />
         <DemoSection />
+        <HowItWorks />
       </div>
     </main>
   );
