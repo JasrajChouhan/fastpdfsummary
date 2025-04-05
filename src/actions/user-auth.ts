158f979d8e5162a232f1f82d9090072623dd1757
Jasraj Chouhan <jasrajc945@gmail.com>
2025-04-06 03:09:37 +0530

    feat(auth-action): add user auth action

---
+++ src/actions/user-auth.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import { auth } from '@clerk/nextjs/server';
+
+export const onAuthenticateUser = async () => {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return {
+        status: 401,
+        message: 'Unauthorized',
+      };
+    }
+
+    return {
+      status: 200,
+      message: 'User is authenticated',
+      data: {
+        userId,
+      },
+    };
+  } catch (error: any) {
+    return {
+      status: 500,
+      message: 'Internal Server Error',
+    };
+  }
+};
