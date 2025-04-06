8633860ffc9abf2862fd9b226c61eee99ba24a9a
Jasraj Chouhan <jasrajc945@gmail.com>
2025-04-07 01:47:42 +0530

    feat(webhook): add update-user clerk webhook.

--- src/actions/clerk-user-action.ts
+++ src/actions/clerk-user-action.ts
@@ -36,3 +36,41 @@ export async function createUser({ email, fullName, primeId }: ClerkUser) {
     throw new Error('User creation failed.');
   }
 }
+
+export async function updateUser({ email, fullName, primeId }: ClerkUser) {
+  try {
+    // Check if user exists
+    const existingUser = await prisma.user.findUnique({
+      where: { primeId, email },
+    });
+
+    if (!existingUser) {
+      return {
+        status: 404,
+        message: 'User not found. Cannot update.',
+      };
+    }
+
+    // Update user with new info
+    const updatedUser = await prisma.user.update({
+      where: { primeId, email },
+      data: {
+        email,
+        fullName,
+      },
+    });
+
+    console.log('[User Updated] ID:', updatedUser.id);
+    return {
+      status: 200,
+      message: 'User updated successfully',
+      data: updatedUser,
+    };
+  } catch (error) {
+    console.error('[Update User Error]:', error);
+    return {
+      status: 500,
+      message: 'Failed to update user',
+    };
+  }
+}
